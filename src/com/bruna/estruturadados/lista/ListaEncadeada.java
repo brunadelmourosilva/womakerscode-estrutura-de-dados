0c7dfe8f0af82a25f45e2c12de04a7ca9010fa22
Alex Rodrigues <brunadelmouro@gmail.com>
2022-02-05 14:02:18 -0300

    aula 27

--- src/com/bruna/estruturadados/lista/ListaEncadeada.java
+++ src/com/bruna/estruturadados/lista/ListaEncadeada.java
@@ -3,10 +3,17 @@ package com.bruna.estruturadados.lista;
 public class ListaEncadeada<T> {
 
     No<T> inicio;
+    private Integer tamanho = 0;
 
     public void adicionaNo(T elemento){
         No<T> no = new No<>(elemento);
         this.inicio = no; //aponta para o início do nó
+
+        this.tamanho++;
+    }
+
+    public Integer getTamanho(){
+        return this.tamanho;
     }
 
     @Override
